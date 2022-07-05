707c1170e7df6c1f4ae27ec29e2a45368150a504
everlaster <everlaster@protonmail.com>
2022-07-05 23:12:54 +0300

    restore CreatePopupAuto as a static extension method

---
+++ src/MVRScriptExtensions.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public static class MVRScriptExtensions
+{
+    public static UIDynamicPopup CreatePopupAuto(
+        this MVRScript script,
+        JSONStorableStringChooser jssc,
+        bool rightSide = false,
+        float popupPanelHeight = 0f,
+        bool upwards = false
+    )
+    {
+        var popup = script.CreateFilterablePopup(jssc, rightSide);
+
+        popup.popup.labelText.alignment = TextAnchor.UpperCenter;
+        popup.popup.labelText.GetComponent<RectTransform>().anchorMax = new Vector2(0, 0.89f);
+
+        {
+            var btn = script.InstantiateButtonTransform();
+            btn.SetParent(popup.transform, false);
+            Object.Destroy(btn.GetComponent<LayoutElement>());
+            btn.GetComponent<UIDynamicButton>().label = "<";
+            btn.GetComponent<UIDynamicButton>().button.onClick.AddListener(() => popup.popup.SetPreviousValue());
+            var prevBtnRect = btn.GetComponent<RectTransform>();
+            prevBtnRect.pivot = new Vector2(0, 0);
+            prevBtnRect.anchoredPosition = new Vector2(10f, 0);
+            prevBtnRect.sizeDelta = new Vector2(0f, 0f);
+            prevBtnRect.offsetMin = new Vector2(5f, 5f);
+            prevBtnRect.offsetMax = new Vector2(80f, 70f);
+            prevBtnRect.anchorMin = new Vector2(0f, 0f);
+            prevBtnRect.anchorMax = new Vector2(0f, 0f);
+        }
+
+        {
+            var btn = script.InstantiateButtonTransform();
+            btn.SetParent(popup.transform, false);
+            Object.Destroy(btn.GetComponent<LayoutElement>());
+            btn.GetComponent<UIDynamicButton>().label = ">";
+            btn.GetComponent<UIDynamicButton>().button.onClick.AddListener(() => popup.popup.SetNextValue());
+            var prevBtnRect = btn.GetComponent<RectTransform>();
+            prevBtnRect.pivot = new Vector2(0, 0);
+            prevBtnRect.anchoredPosition = new Vector2(10f, 0);
+            prevBtnRect.sizeDelta = new Vector2(0f, 0f);
+            prevBtnRect.offsetMin = new Vector2(82f, 5f);
+            prevBtnRect.offsetMax = new Vector2(157f, 70f);
+            prevBtnRect.anchorMin = new Vector2(0f, 0f);
+            prevBtnRect.anchorMax = new Vector2(0f, 0f);
+        }
+
+        if(popupPanelHeight > 0f)
+        {
+            popup.popupPanelHeight = popupPanelHeight;
+        }
+
+        if(upwards)
+        {
+            popup.popup.popupPanel.offsetMin += new Vector2(0, popup.popupPanelHeight + 60);
+            popup.popup.popupPanel.offsetMax += new Vector2(0, popup.popupPanelHeight + 60);
+        }
+
+        return popup;
+    }
+
+    private static Transform InstantiateButtonTransform(this MVRScript script) => Object
+        .Instantiate(script.manager.configurableButtonPrefab);
+}
